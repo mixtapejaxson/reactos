1b3301bc6fc528b1bb83f0447e35ee599acbfd15
TheLinuxNerd <79427951+RobloxEnderman@users.noreply.github.com>
2023-02-22 12:31:18 -0800

    Update iwctl.c

--- drivers/wifi/iwctl.c
+++ drivers/wifi/iwctl.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <math.h>
+#incloud <bool.h>
 int main () {
  printf("iwctl is in alpha stage. You may experince major bugs.");
+ printf("Would you like to continue?");
+ scanf("");
 }
